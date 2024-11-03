20005f513aeede2b266e1747ad25e856059fe9b7
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2024-11-02 21:27:43 -0500

    hola mundo, operaciones y tipos de datos

---
+++ hello.py
@@ -0,0 +1,61 @@
+# Esto es un comentario
+# Nuestro hola mundo en python
+
+print("Hola Python")
+print('Hola Python')
+
+"""
+Esto es un
+comentario
+en varias líneas
+"""
+
+'''
+Esto también es un
+comentario
+en varias líneas
+'''
+
+# Operaciones
+print(2 + 3) # Suma
+print(3 - 5) # Resta
+print(2 * 9) # Multiplicación
+print(3 / 2) # Division
+print(3 ** 2) # Potenciación
+print(7 % 3) # Residuo
+print(7 // 3) # Cociente
+
+# Consultar el tipo de dato
+print(type("Hola Python")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(1 + 3j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+print(type([1, 2, 3])) # Tipo 'list'
+print(
+    type(
+        {
+            'name': 'Gian',
+            'edad': 24
+        }
+    )
+) # Tipo 'dict'
+print(
+    type(
+        {
+            9.8,
+            3.14,
+            2.45
+        }
+    )
+) # Tipo 'set'
+print(
+    type(
+        (
+            9.8,
+            3.14,
+            2.45
+        )
+    )
+) # Tipo 'tuple'
+
